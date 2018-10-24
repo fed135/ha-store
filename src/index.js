6c2d65b9373a3c2250323f73da45d2b383735192
Damon Perron-Laurin <dperronlaurin@shutterstock.com>
2018-10-24 17:27:21 -0400

    Convert src/index.js to class for easier parsing and reading Also add a slight gain in perf Add tests for config parsing

--- src/index.js
+++ src/index.js
@@ -1,143 +1,108 @@
-/** 
+/**
  * Batcher index
  */
 
 /* Requires ------------------------------------------------------------------*/
-
-const q = require('./queue.js');
-const l = require('./store.js');
-const b = require('./breaker.js');
-const { exp, contextKey, recordKey } = require('./utils.js');
-const { randomBytes } = require('crypto');
+const queue = require('./queue.js');
+const store = require('./store.js');
+const breaker = require('./breaker.js');
+const {contextKey, recordKey} = require('./utils.js');
+const {randomBytes} = require('crypto');
 const EventEmitter = require('events').EventEmitter;
+const {hydrateConfig} = require('./options');
 
 /* Local variable ------------------------------------------------------------*/
 
-const baseConfig = {
-  batch: {
-    tick: 50,
-    max: 100,
-  },
-  retry: {
-    base: 5,
-    steps: 3,
-    limit: 5000,
-    curve: exp,
-  },
-  cache: {
-    base: 1000,
-    steps: 5,
-    limit: 30000,
-    curve: exp,
-  },
-  breaker: {
-    base: 1000,
-    steps: 10,
-    limit: 0xffff,
-    curve: exp,
-    tolerance: 1,
-    toleranceFrame: 10000,
-  },
-}
+class HaStore extends EventEmitter {
+  constructor(initialConfig = {}, emitter = new EventEmitter()) {
+    super();
 
-/* Methods -------------------------------------------------------------------*/
+    // Parameter validation
+    if (typeof initialConfig.resolver !== 'function') {
+      throw new Error(`config.resolver [${initialConfig.resolver}] is not a function`);
+    }
 
-/**
- * Batcher constructor 
- * @class batcher
- */
-function batcher(config = {}, emitter) {
-  // Parameter validation
-  if (typeof config.resolver !== 'function') {
-    throw new Error(`config.resolver [${config.resolver}] is not a function`);
+    if (emitter && !emitter.emit) {
+      throw new Error(`${emitter} is not an EventEmitter`);
+    }
+
+    this.config = hydrateConfig(initialConfig);
+
+    // Local variables
+    if (this.setMaxListeners) {
+      this.setMaxListeners(Infinity);
+    }
+
+    const circuitBreaker = breaker(this.config, this);
+    this.breaker = {
+      ...circuitBreaker,
+      open: circuitBreaker.openCircuit,
+      close: circuitBreaker.restoreCircuit
+    };
+
+    this.queue = queue(this.config, this, store(this.config, this), this.config.store, circuitBreaker);
   }
-
-  config.storeOptions = config.storeOptions || {};
-  config.timeout = Number(config.timeout) || null; 
-  config.storeOptions.pluginRecoveryDelay = Number(config.storeOptions.pluginRecoveryDelay) || 10000;
-  config.storeOptions.pluginFallback = (config.storeOptions.pluginFallback === undefined) ? true : config.storeOptions.pluginFallback;
-  config.storeOptions.memoryLimit = Math.max(0, Math.min(1, Number(config.storeOptions.memoryLimit) || 0.9));
-  config.storeOptions.recordLimit = Number(config.storeOptions.recordLimit) || Infinity;
-
-  if (config.batch !== null) config.batch = { ...baseConfig.batch, ...config.batch };
-  if (config.retry !== null) config.retry = { ...baseConfig.retry, ...config.retry };
-  if (config.cache !== null) config.cache = { ...baseConfig.cache, ...config.cache };
-  if (config.breaker !== null) config.breaker = { ...baseConfig.breaker, ...config.breaker };
-
-  if (emitter && !emitter.emit) {
-    throw new Error(`${emitter} is not an EventEmitter`);
-  }
-
-  // Local variables
-  const _emitter = emitter || new EventEmitter();
-  _emitter.setMaxListeners && _emitter.setMaxListeners(Infinity);
-  const _breaker = b(config, _emitter);
-
-  const _queue = q(config, _emitter, l(config, _emitter), config.store, _breaker);
 
   /**
-     * Gets a list of records from source
-     * @param {string|number|array<string|number>} ids The id of the record to fetch
-     * @param {object} params (Optional)The Request parameters
-     * @returns {Promise} The eventual single record
-     */
-  function get(ids, params = {}, agg = null) {
+   * Gets a list of records from source
+   * @param {string|number|array<string|number>} ids The id of the record to fetch
+   * @param {object} params (Optional)The Request parameters
+   * @returns {Promise} The eventual single record
+   */
+  get(ids, params = {}, agg = null) {
     if (params === null) params = {};
     const uid = randomBytes(8).toString('hex');
     const requestIds = (Array.isArray(ids)) ? ids : [ids];
     const promises = requestIds.map((id, i) => {
-      return _queue.push(id, params, agg, (config.batch === null && i === requestIds.length -1), uid);
+      return this.queue.push(id, params, agg, (this.config.batch === null && i === requestIds.length - 1), uid);
     });
     return Promise.all(promises)
       .then(response => (!Array.isArray(ids)) ? response[0] : response);
   }
 
   /**
-     * Inserts results into cache manually
-     * @param {*} items Raw results from a data-source to load into cache
-     * @param {array<string|number>} ids The id(s) to extract from the raw dataset
-     * @param {object} params (Optional)The Request parameters
-     * @returns {Promise} The eventual single record
-     */
-  function set(items, ids, params = {}) {
+   * Inserts results into cache manually
+   * @param {*} items Raw results from a data-source to load into cache
+   * @param {array<string|number>} ids The id(s) to extract from the raw dataset
+   * @param {object} params (Optional)The Request parameters
+   * @returns {Promise} The eventual single record
+   */
+
+  set(items, ids, params = {}) {
     if (!Array.isArray(ids) || ids.length === 0) throw new Error('Missing required argument id list in batcher #set. ');
-    const key = contextKey(config.uniqueParams, params);
-    return _queue.complete(key, ids, _queue.resolveContext(key, params), items);
+    const key = contextKey(this.config.uniqueParams, params);
+    return this.queue.complete(key, ids, this.queue.resolveContext(key, params), items);
   }
 
   /**
-     * Clears one or more recors from temp store 
-     * @param {string|number|array<string|number>} ids The id(s) to clear
-     * @param {object} params (Optional) The Request parameters
-     * @returns {boolean} The result of the clearing
-     */
-  function clear(ids, params) {
-    if (Array.isArray(ids)) return ids.map(id => clear(id, params));
-    return _queue.store.clear(recordKey(contextKey(config.uniqueParams, params), ids));
+   * Clears one or more recors from temp store
+   * @param {string|number|array<string|number>} ids The id(s) to clear
+   * @param {object} params (Optional) The Request parameters
+   * @returns {boolean} The result of the clearing
+   */
+  clear(ids, params) {
+    if (Array.isArray(ids)) {
+      return ids.map(id => this.clear(id, params));
+    }
+
+    return this.queue.store.clear(recordKey(contextKey(this.config.uniqueParams, params), ids));
   }
 
   /**
-     * Returns the amount of records and contexts in memory
-     * @returns {object}
-     */
-  async function size() {
-    return await {
-      contexts: _queue.size(),
-      records: await _queue.store.size(),
-    }; 
+   * Returns the amount of records and contexts in memory
+   * @returns {object}
+   */
+  async size() {
+    return {
+      contexts: this.queue.size(),
+      records: await this.queue.store.size(),
+    };
   }
-
-  return Object.assign(_emitter, {
-    get,
-    set,
-    clear,
-    size,
-    config,
-    breaker: { open: _breaker.openCircuit, close: _breaker.restoreCircuit },
-    _queue
-  });
 }
 
 /* Exports -------------------------------------------------------------------*/
+function make(initialConfig = {}, emitter = new EventEmitter()) {
+  return new HaStore(initialConfig, emitter);
+}
 
-module.exports = batcher;
+module.exports = make;
