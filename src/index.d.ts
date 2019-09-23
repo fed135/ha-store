d867efee80afc74f8c9eea175ba5eac0b97bf7df
Frederic Charette <fredericcharette@gmail.com>
2019-09-23 09:35:46 -0400

    Added missing external store config

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,6 +7,15 @@ type Params = {
 type RequestIds = string | number | string[] | number[]
 type Serializable = string | number | boolean | { [key: string]: Serializable } | Array<Serializable>
 
+export type HAExternalStore = {
+  get: (key: string) => Promise<Serializable>
+  getMulti: (recordKey: (contextKey: string) => string, keys: RequestIds) => Promise<Serializable[]>
+  set: (recordKey: (contextKey: string) => string, keys: RequestIds, values: Serializable) => Promise<Serializable>
+  clear: (key?: string) => boolean
+  size: () => number
+  connection?: any
+}
+
 export interface HAStoreConfig {
   resolver(ids: RequestIds, params?: Params, context?: Serializable): Promise<Serializable>
   uniqueOptions?: string[]
@@ -22,12 +31,13 @@ export interface HAStoreConfig {
   batch?: {
     tick?: number
     max?: number
-  }
+  },
+  store: HAExternalStore
 }
 
 export type HAStore = {
-  get(ids: string | number, params?: Params): Promise<any>
-  set(items: Serializable, ids: string[] | number[], params?: Params): Promise<any>
+  get(ids: string | number, params?: Params): Promise<Serializable>
+  set(items: Serializable, ids: string[] | number[], params?: Params): Promise<Serializable>
   clear(ids: RequestIds, params?: Params): void
   size(): { contexts: number, queries: number, records: number }
   getKey(id: string | number, params?: Params): string
